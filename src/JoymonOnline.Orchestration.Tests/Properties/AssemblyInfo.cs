fabbd0112fbf96a90e6c4c1352e534d32e8d4f91
Joymon <joymon@gmail.com>
2015-06-25 18:29:03 -0400

    Changed the assembly version

--- src/JoymonOnline.Orchestration.Tests/Properties/AssemblyInfo.cs
+++ src/JoymonOnline.Orchestration.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JoymonOnline.Orchestration.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("kpmg")]
+[assembly: AssemblyCompany("JoymonOnline")]
 [assembly: AssemblyProduct("JoymonOnline.Orchestration.Tests")]
-[assembly: AssemblyCopyright("Copyright © kpmg 2015")]
+[assembly: AssemblyCopyright("Copyright © JoymonOnline 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
