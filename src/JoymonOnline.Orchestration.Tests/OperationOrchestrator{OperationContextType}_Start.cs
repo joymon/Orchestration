f4044a941f548edae393dd4551b5cb316cfe3ec4
joymon <joymon@gmail.com>
2015-11-01 12:52:33 -0500

    Added one more test case

--- src/JoymonOnline.Orchestration.Tests/OperationOrchestrator{OperationContextType}_Start.cs
+++ src/JoymonOnline.Orchestration.Tests/OperationOrchestrator{OperationContextType}_Start.cs
@@ -9,7 +9,7 @@ using System.Text;
 namespace JoymonOnline.Orchestration.Tests
 {
     [TestClass]
-    public class OperationOrchestrator_OperationContextType_Tests
+    public class OperationOrchestrator_OperationContextType_Start
     {
         [TestMethod]
         public void WhenInNormalCondition_ShouldWork()
@@ -29,12 +29,44 @@ namespace JoymonOnline.Orchestration.Tests
             orchestrator.Start(10);
         }
         [TestMethod]
+        public void WhenContextIsProvided_ShouldFindSumAndAverage()
+        {
+            IOperationOrchestrator<CalculationContext> orchestrator = new OperationOrchestrator<CalculationContext>(
+                new List<IOperation<CalculationContext>>() {
+                    new FindSumOperation(),
+                    new FindAverageOperation()
+                });
+            CalculationContext context = new CalculationContext() { Numbers = new int[] { 1, 2, 3, 6 } };
+            orchestrator.Start(context);
+            Console.WriteLine("Sum={0},Average={1}", context.Sum, context.Average);
+        }
+        [TestMethod]
         public void WhenBackgroundOperationsListProviderIsUsed_ShouldSucceed()
         {
             IOperationOrchestrator<int> orchestrator =
                 new OperationOrchestrator<int>(new OperationsProvider());
             orchestrator.Start(10);
         }
+    }
+    internal class FindSumOperation : IOperation<CalculationContext>
+    {
+        void IOperation<CalculationContext>.Execute(CalculationContext context)
+        {
+            context.Sum = context.Numbers.Sum();
+        }
+    }
+    internal class FindAverageOperation : IOperation<CalculationContext>
+    {
+        void IOperation<CalculationContext>.Execute(CalculationContext context)
+        {
+            context.Average = context.Sum / context.Numbers.Length;
+        }
+    }
+    internal class CalculationContext
+    {
+        public int[] Numbers { get; set; }
+        public int Sum { get; set; }
+        public int Average { get; set; }
     }
 
     internal class FindSquareRoot : IOperation<int>
