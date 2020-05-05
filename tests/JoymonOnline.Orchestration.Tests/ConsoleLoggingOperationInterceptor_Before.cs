55216964f0a879888953dfafee1b670a6127d502
Joy George Kunjikkuru <joymon@gmail.com>
2020-05-05 07:55:16 -0400

    Code coverage - ConsoleloggingOperationInterceptor

--- tests/JoymonOnline.Orchestration.Tests/ConsoleLoggingOperationInterceptor_Before.cs
+++ tests/JoymonOnline.Orchestration.Tests/ConsoleLoggingOperationInterceptor_Before.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using JoymonOnline.Orchestration.Core;
+using JoymonOnline.Orchestration.Interceptors;
+using JoymonOnline.Orchestration.Orchestrators;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace JoymonOnline.Orchestration.Tests
@@ -6,5 +10,16 @@ namespace JoymonOnline.Orchestration.Tests
     [TestClass]
     public class ConsoleLoggingOperationInterceptor_Before
     {
+        [TestMethod]
+        public void WhenOneInterceptorIsGiven_ItShouldWork()
+        {
+            TestInterceptor interceptor = new TestInterceptor();
+            IOperationOrchestrator<int> orch = new InterceptableOperationOrchestrator<int> (
+                new OperationsProvider(),
+                new List<IOperationInterceptable<int>>() { new ConsoleLoggingOperationInterceptor<int>() }
+                );
+
+            orch.Start(20);
+        }
     }
 }
